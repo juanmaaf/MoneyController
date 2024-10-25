7ee571f76b5c46229ceb08994496c37edb739967
Matteo <1000014829@studium.unict.it>
2024-10-25 13:37:14 +0200

    update

--- money_controller/gasto.py
+++ money_controller/gasto.py
@@ -1,3 +1,6 @@
+#¿Cómo podemos categorizar los gastos en fijos y variables?
+#[HU1]
+
 class Gasto:
     def __init__(self, descripcion, monto, fijo=True):
         self.descripcion = descripcion
--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -1,3 +1,9 @@
+#¿Cómo podemos calcular los ahorros disponibles cada mes?
+#[HU1]
+#¿Cómo podemos evaluar si un gasto adicional es viable? 
+#[HU2]
+from money_controller.gasto import Gasto
+
 class Presupuesto:
     def __init__(self, monto_total):
         self.monto_total = monto_total
