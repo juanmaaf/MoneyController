f8a7140db8dca33edc2bd5a4564aa52df9b5f7ab
MatteoImbrosciano <82762466+MatteoImbrosciano@users.noreply.github.com>
2024-10-28 19:01:13 +0100

    Delete build directory

--- build/lib/money_controller/gasto.py
+++
@@ -1,8 +0,0 @@
-class Gasto:
-    def __init__(self, descripcion, monto, fijo=True):
-        self.descripcion = descripcion
-        self.monto = monto
-        self.fijo = fijo
-    
-    def es_fijo(self):
-        return self.fijo
