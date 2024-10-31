7e9d403c4825c409e37ea36cf487abed13a2f619
Matteo <1000014829@studium.unict.it>
2024-10-31 10:22:44 +0100

    issue #9, #10 Decidí usar las clases de datos de Python para hacer que los valores sean inmutables porque antes no lo eran.

--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -1,15 +1,14 @@
 #¿Cómo podemos calcular los ahorros disponibles cada mes?
 #[HU1]
 #¿Cómo podemos evaluar si un gasto adicional es viable? 
-#[HU2]
+# money_controller/presupuesto.py
 from money_controller.gasto import Gasto
+from dataclasses import dataclass, field
+from typing import List
 
+@dataclass
 class Presupuesto:
-    def __init__(self, monto_total):
 
-        if monto_total <= 0:
-            raise ValueError("El presupuesto total debe ser un valor positivo")
-        
-        self.monto_total = monto_total
-        self.gastos = []
-        self.ingresos = []
+    monto_total: float                                   # Budget totale mensile
+    gastos: List[Gasto] = field(default_factory=list)    # Lista di spese
+    ingresos: List[float] = field(default_factory=list)  # Lista di entrate
