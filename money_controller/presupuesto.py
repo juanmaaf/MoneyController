0bb6d37f088ee88eeeae93a888bfb3361149a57f
MatteoImbrosciano <82762466+MatteoImbrosciano@users.noreply.github.com>
2024-10-31 09:35:58 +0100

    Update presupuesto.py

--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -6,6 +6,10 @@ from money_controller.gasto import Gasto
 
 class Presupuesto:
     def __init__(self, monto_total):
+
+        if monto_total <= 0:
+            raise ValueError("El presupuesto total debe ser un valor positivo")
+        
         self.monto_total = monto_total
         self.gastos = []
         self.ingresos = []
