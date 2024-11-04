48881bb6fc9ab99898add3c6af0077338db610b8
MatteoImbrosciano <82762466+MatteoImbrosciano@users.noreply.github.com>
2024-11-04 18:07:57 +0100

    Update presupuesto.py

--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -1,14 +1,17 @@
-#¿Cómo podemos calcular los ahorros disponibles cada mes?
-#[HU1]
-#¿Cómo podemos evaluar si un gasto adicional es viable? 
-# money_controller/presupuesto.py
 from money_controller.gasto import Gasto
 from dataclasses import dataclass, field
 from typing import List
 
 @dataclass
 class Presupuesto:
+    """
+    Representa el presupuesto mensual, que incluye tanto los gastos como los ingresos.
 
-    monto_total: float                                   # Budget totale mensile
-    gastos: List[Gasto] = field(default_factory=list)    # Lista di spese
-    ingresos: List[float] = field(default_factory=list)  # Lista di entrate
+    Atributos:
+        monto_total (float): El presupuesto total mensual.
+        gastos (List[Gasto]): Lista de gastos realizados.
+        ingresos (List[float]): Lista de ingresos.
+    """
+    monto_total: float
+    gastos: List[Gasto] = field(default_factory=list)
+    ingresos: List[float] = field(default_factory=list)
