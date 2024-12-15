ead0665724898d81c668e69fc5b7b5153d16f157
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-12-15 18:18:03 +0100

    Ayuda a resolver #20. Creo función que leerá archivo csv con los gastos sin emplear bibliotecas externas

--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -22,3 +22,18 @@ class Presupuesto:
     gastos_variables: List[Gasto] = field(default_factory=list)
     ingresos: List[float] = field(default_factory=list)
     gasto_no_planificado: Optional[float] = None
+    
+    
+def leer_archivo_csv(ruta_archivo):
+    try:
+        with open(ruta_archivo, "r", encoding="utf-8") as archivo:
+            lineas = archivo.readlines()
+            if not lineas:
+                raise ValueError("El archivo está vacío.")
+            return lineas
+    except FileNotFoundError:
+        raise ValueError(f"El archivo {ruta_archivo} no existe")
+    except ValueError as e:
+        raise e
+    except Exception as e:
+        raise RuntimeError(f"Error al leer el archivo: {e}")
