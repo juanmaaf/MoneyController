4d13b4670b5fdd92880c7291867573b517bc6e70
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-12-15 18:44:15 +0100

    Ayuda a resolver #21. Ajusto código de gasto.py adaptándolo al contenido de los datos obtenidos del CSV.

--- money_controller/gasto.py
+++ money_controller/gasto.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
 from money_controller.categoriaGasto import CategoriaGasto
+from datetime import datetime
+
 @dataclass
 class Gasto:
-    """
-    Representa un gasto en el presupuesto mensual.
-
-    Atributos:
-        descripcion (str): La descripción del gasto.
-        monto (float): La cantidad del gasto.
-        categoria (CategoriaGasto): La categoría del gasto, fija o variable.
-    """
-    descripcion: str
-    monto: float
+    fecha: datetime
+    concepto: str
+    importe: float
     categoria: CategoriaGasto = CategoriaGasto
+
+    def __post_init__(self):
+        if isinstance(self.fecha, str):
+            try:
+                self.fecha = datetime.strptime(self.fecha, '%Y-%m-%d')
+            except ValueError:
+                raise ValueError(f"La fecha {self.fecha} no está en el formato correcto (YYYY-MM-DD).")
