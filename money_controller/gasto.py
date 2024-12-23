b74ff1c4b179e87e1d645b44930ac61e37306365
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-12-23 14:54:40 +0100

    closes #20, #21 y #22. Issues incorrectos. Empiezo de 0 el objetivo. Cambios no justificados.

--- money_controller/gasto.py
+++ money_controller/gasto.py
@@ -1,17 +1,15 @@
 from dataclasses import dataclass
 from money_controller.categoriaGasto import CategoriaGasto
-from datetime import datetime
-
 @dataclass
 class Gasto:
-    fecha: datetime
-    concepto: str
-    importe: float
-    categoria: CategoriaGasto = CategoriaGasto
+    """
+    Representa un gasto en el presupuesto mensual.
 
-    def __post_init__(self):
-        if isinstance(self.fecha, str):
-            try:
-                self.fecha = datetime.strptime(self.fecha, '%Y-%m-%d')
-            except ValueError:
-                raise ValueError(f"La fecha {self.fecha} no está en el formato correcto (YYYY-MM-DD).")
+    Atributos:
+        descripcion (str): La descripción del gasto.
+        monto (float): La cantidad del gasto.
+        categoria (CategoriaGasto): La categoría del gasto, fija o variable.
+    """
+    descripcion: str
+    monto: float
+    categoria: CategoriaGasto = CategoriaGasto
--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -1,95 +1,24 @@
 from money_controller.gasto import Gasto
 from dataclasses import dataclass, field
 from typing import List, Optional
-from datetime import datetime
-from money_controller.categoriaGasto import CategoriaGasto
 
 @dataclass
 class Presupuesto:
+    
+    """
+    Representa el presupuesto mensual, que incluye tanto los gastos como los ingresos.
+
+    Atributo:
+        monto_total (float): El presupuesto total mensual.
+        gastos_fijos (List[Gasto]): Lista de gastos fijos (ej. alquiler, internet).
+
+        gastos_variables (List[Gasto]): Lista de gastos variables (ej.coomida, entretenimiento).
+
+        ingresos (List[float]): Lista de ingresos.
+        
+    """
     monto_total: float
-    meta_ahorro: float = None
     gastos_fijos: List[Gasto] = field(default_factory=list)
     gastos_variables: List[Gasto] = field(default_factory=list)
     ingresos: List[float] = field(default_factory=list)
     gasto_no_planificado: Optional[float] = None
-    
-    def __post_init__(self):
-        self.gastos_fijos = [gasto for gasto in self.gastos_fijos if isinstance(gasto, Gasto)]
-        self.gastos_variables = [gasto for gasto in self.gastos_variables if isinstance(gasto, Gasto)]
-    
-def leer_archivo_csv(ruta_archivo):
-    try:
-        with open(ruta_archivo, "r", encoding="utf-8") as archivo:
-            lineas = archivo.readlines()
-            if not lineas:
-                raise ValueError("El archivo está vacío.")
-            return lineas
-    except FileNotFoundError:
-        raise ValueError(f"El archivo {ruta_archivo} no existe")
-    except ValueError as e:
-        raise e
-    except Exception as e:
-        raise RuntimeError(f"Error al leer el archivo: {e}")
-
-def procesar_atributos(fecha_str, importe_str):
-    fecha = datetime.strptime(fecha_str, "%Y-%m-%d")
-    importe = float(importe_str)
-    return fecha, importe
-
-def procesar_gasto(presupuesto, gastos_vistos, concepto, categoria_str, importe, fecha):
-    clave_gasto = (concepto, categoria_str, importe)
-    
-    if clave_gasto in gastos_vistos:
-        for gasto in presupuesto.gastos_variables:
-            if gasto.concepto == concepto and gasto.importe == importe and gasto.categoria == CategoriaGasto.VARIABLE:
-                gasto.categoria=CategoriaGasto.FIJO
-                presupuesto.gastos_variables.remove(gasto)
-                presupuesto.gastos_fijos.append(gasto)
-        gasto = Gasto(fecha=fecha, concepto=concepto, importe=importe, categoria=CategoriaGasto.FIJO)
-        presupuesto.gastos_fijos.append(gasto)
-    else:
-        gasto = Gasto(fecha=fecha, concepto=concepto, importe=importe, categoria=CategoriaGasto.VARIABLE)
-        presupuesto.gastos_variables.append(gasto)
-        gastos_vistos[clave_gasto] = True
-
-def actualizar_monto_total(presupuesto):
-    total_gastos = sum(gasto.importe for gasto in presupuesto.gastos_fijos) + sum(gasto.importe for gasto in presupuesto.gastos_variables)
-    presupuesto.monto_total = sum(presupuesto.ingresos) - total_gastos
-
-def procesar_datos(ruta_archivo):
-    lineas = leer_archivo_csv(ruta_archivo)
-    presupuesto = Presupuesto(monto_total=0)
-    gastos_vistos = {}
-
-    for i, linea in enumerate(lineas):
-        if i == 0: 
-            continue
-        atributos = linea.strip().split(",")
-        if len(atributos) != 5:
-            continue
-        
-        fecha_str, concepto, categoria_str, importe_str, tipo_movimiento = atributos
-        
-        try:
-            fecha, importe = procesar_atributos(fecha_str, importe_str)
-        except ValueError as e:
-            raise ValueError(f"Error al procesar la línea: {linea}. Detalle del error: {e}")
-
-        if tipo_movimiento == "Ingreso":
-            presupuesto.ingresos.append(importe)
-        elif tipo_movimiento == "Gasto":
-            procesar_gasto(presupuesto, gastos_vistos, concepto, categoria_str, importe, fecha)
-
-    actualizar_monto_total(presupuesto)
-    return presupuesto
-
-def puede_permitirse_gasto_adicional(presupuesto, importe_adicional: float) -> bool:
-    total_gastos = sum(gasto.importe for gasto in presupuesto.gastos_fijos) + sum(gasto.importe for gasto in presupuesto.gastos_variables)
-    
-    ingreso_disponible = sum(presupuesto.ingresos) - total_gastos - presupuesto.meta_ahorro
-    
-    if ingreso_disponible >= importe_adicional:
-        presupuesto.gasto_no_planificado = importe_adicional
-        return True
-    else:
-        return False
