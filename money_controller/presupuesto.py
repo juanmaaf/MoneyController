01ddc30e22255269931b6e151c02eb91b47fc0e0
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-12-23 15:22:25 +0100

    Resuelve #21. Defino método para procesar los datos léidos dividido en submétodos legibles.

--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -1,6 +1,8 @@
 from money_controller.gasto import Gasto
 from dataclasses import dataclass, field
 from typing import List, Optional
+from datetime import datetime
+from money_controller.categoriaGasto import CategoriaGasto
 
 @dataclass
 class Presupuesto:
@@ -40,3 +42,55 @@ def leer_archivo_csv(ruta_archivo):
         raise e
     except Exception as e:
         raise RuntimeError(f"Error al leer el archivo: {e}")
+    
+def procesar_atributos(fecha_str, monto_str):
+    fecha = datetime.strptime(fecha_str, "%Y-%m-%d")
+    monto = float(monto_str)
+    return fecha, monto
+
+def procesar_gasto(presupuesto, gastos_vistos, descripcion, categoria_str, monto, fecha):
+    clave_gasto = (descripcion, categoria_str, monto)
+    
+    if clave_gasto in gastos_vistos:
+        for gasto in presupuesto.gastos_variables:
+            if gasto.descripcion == descripcion and gasto.monto == monto and gasto.categoria == CategoriaGasto.VARIABLE:
+                gasto.categoria=CategoriaGasto.FIJO
+                presupuesto.gastos_variables.remove(gasto)
+                presupuesto.gastos_fijos.append(gasto)
+        gasto = Gasto(descripcion=descripcion, monto=monto, categoria=CategoriaGasto.FIJO, fecha=fecha)
+        presupuesto.gastos_fijos.append(gasto)
+    else:
+        gasto = Gasto(descripcion=descripcion, monto=monto, categoria=CategoriaGasto.VARIABLE, fecha=fecha)
+        presupuesto.gastos_variables.append(gasto)
+        gastos_vistos[clave_gasto] = True
+
+def actualizar_monto_total(presupuesto):
+    total_gastos = sum(gasto.monto for gasto in presupuesto.gastos_fijos) + sum(gasto.monto for gasto in presupuesto.gastos_variables)
+    presupuesto.monto_total = sum(presupuesto.ingresos) - total_gastos
+
+def procesar_datos(ruta_archivo):
+    lineas = leer_archivo_csv(ruta_archivo)
+    presupuesto = Presupuesto(monto_total=0)
+    gastos_vistos = {}
+
+    for i, linea in enumerate(lineas):
+        if i == 0: 
+            continue
+        atributos = linea.strip().split(",")
+        if len(atributos) != 5:
+            continue
+        
+        descripcion, categoria_str, monto_str, tipo_movimiento, fecha_str = atributos
+        
+        try:
+            fecha, monto = procesar_atributos(fecha_str, monto_str)
+        except ValueError as e:
+            raise ValueError(f"Error al procesar la línea: {linea}. Detalle del error: {e}")
+
+        if tipo_movimiento == "Ingreso":
+            presupuesto.ingresos.append(monto)
+        elif tipo_movimiento == "Gasto":
+            procesar_gasto(presupuesto, gastos_vistos, descripcion, categoria_str, monto, fecha)
+
+    actualizar_monto_total(presupuesto)
+    return presupuesto
