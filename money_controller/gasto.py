73fa549e06f8530b93e25d1f5622d92a7f447740
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-12-23 15:11:41 +0100

    Resuelve #23. Añado atributo fecha necesario para listar los gastos y una comprobación/validación del formato de ese atributo.

--- money_controller/gasto.py
+++ money_controller/gasto.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from money_controller.categoriaGasto import CategoriaGasto
+from datetime import datetime
+
 @dataclass
 class Gasto:
     """
@@ -9,7 +11,16 @@ class Gasto:
         descripcion (str): La descripción del gasto.
         monto (float): La cantidad del gasto.
         categoria (CategoriaGasto): La categoría del gasto, fija o variable.
+        fecha (datetime): La fecha registrada del gasto
     """
     descripcion: str
     monto: float
     categoria: CategoriaGasto = CategoriaGasto
+    fecha: datetime
+    
+    def __post_init__(self):
+        if isinstance(self.fecha, str):
+            try:
+                self.fecha = datetime.strptime(self.fecha, '%Y-%m-%d')
+            except ValueError:
+                raise ValueError(f"La fecha {self.fecha} no está en el formato correcto (YYYY-MM-DD).")
