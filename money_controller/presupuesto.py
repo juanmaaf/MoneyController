9dbb5ee465ede3c77ce51deb48ec8a7ee38edc67
juanmaaf <juanmanuelaneasfranco@gmail.com>
2025-01-08 17:42:31 +0100

    closes #23. Issue incorrecto -> Código Objetivo 2

--- money_controller/presupuesto.py
+++ money_controller/presupuesto.py
@@ -1,36 +1,24 @@
 from money_controller.gasto import Gasto
 from dataclasses import dataclass, field
 from typing import List, Optional
-from datetime import datetime
-from money_controller.categoriaGasto import CategoriaGasto
 
 @dataclass
 class Presupuesto:
-    
+
     """
     Representa el presupuesto mensual, que incluye tanto los gastos como los ingresos.
-
     Atributo:
         monto_total (float): El presupuesto total mensual.
         gastos_fijos (List[Gasto]): Lista de gastos fijos (ej. alquiler, internet).
-
         gastos_variables (List[Gasto]): Lista de gastos variables (ej.coomida, entretenimiento).
-
         ingresos (List[float]): Lista de ingresos.
-        
-        meta_ahorro (float): Ahorro deseado por el usuario
         
     """
     monto_total: float
     gastos_fijos: List[Gasto] = field(default_factory=list)
     gastos_variables: List[Gasto] = field(default_factory=list)
     ingresos: List[float] = field(default_factory=list)
-    meta_ahorro: float = None
     gasto_no_planificado: Optional[float] = None
-    
-    def __post_init__(self):
-        self.gastos_fijos = [gasto for gasto in self.gastos_fijos if isinstance(gasto, Gasto)]
-        self.gastos_variables = [gasto for gasto in self.gastos_variables if isinstance(gasto, Gasto)]
 
 def leer_archivo_csv(ruta_archivo):
     try:
