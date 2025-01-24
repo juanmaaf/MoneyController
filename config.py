d2cd5f018c67ecca18c3b5132a7a17f467c3d278
juanmaaf <juanmanuelaneasfranco@gmail.com>
2025-01-24 23:06:53 +0100

    Resuelve #47. Añado logger en conf

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 
 from dynaconf import Dynaconf
+from logger import init_logger
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=['settings.toml', '.secrets.toml'],
 )
+
+init_logger()
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
