b570f8a859422c6f6efb43c23a9995cc13013711
juanmaaf <juanmanuelaneasfranco@gmail.com>
2025-01-24 23:05:12 +0100

    Resuelve #46. Defino conf logger

---
+++ logger.py
@@ -0,0 +1,15 @@
+from loguru import logger
+import sys
+from config import settings
+
+def init_logger():
+    log_level = settings.get("log.level", "INFO")
+    log_file = settings.get("log.logfile", "money_controller.log")
+
+    logger.remove()
+
+    logger.add(sys.stdout, level=log_level, format="{time} | {level} | {message}")
+
+    logger.add(log_file, level=log_level, format="{time} | {level} | {message}", rotation="1 week")
+
+init_logger()
